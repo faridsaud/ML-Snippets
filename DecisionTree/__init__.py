9d28926b50629cf15ad8a5f8bb66747c0078e1a1
Farid Saud <faridsaud93@gmail.com>
2019-05-14 18:02:52 -0500

    [Feat]: Update Decision Tree

--- DecisionTree/__init__.py
+++ DecisionTree/__init__.py
@@ -1,6 +1,7 @@
 # Import statements 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import pandas as pd
 import numpy as np
 
@@ -10,12 +11,27 @@ data = np.asarray(pd.read_csv('data.csv', header=None))
 X = data[:,0:2]
 y = data[:,2]
 
+print(X.shape)
+
+print(y.shape)
+
+# split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    random_state=1)
+
 # Create model
 model = DecisionTreeClassifier()
 
 # Fit model
-model.fit(X, y)
+model.fit(X_train, y_train)
 
-y_pred = model.predict(X)
+# Predict results
+y_pred = model.predict(X_test)
 
-acc = accuracy_score(y, y_pred)
+# See accuracy
+
+print('Accuracy score: ', format(accuracy_score(y_test, y_pred)))
+print('Precision score: ', format(precision_score(y_test, y_pred)))
+print('Recall score: ', format(recall_score(y_test, y_pred)))
+print('F1 score: ', format(f1_score(y_test, y_pred)))
