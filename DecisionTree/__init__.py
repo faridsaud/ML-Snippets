765e29013d92da72d29eab95f348b4a9f6ad8bc3
Farid Saud <faridsaud93@gmail.com>
2019-05-14 13:32:09 -0500

    [Feat]: Add Decision Tree

---
+++ DecisionTree/__init__.py
@@ -0,0 +1,21 @@
+# Import statements 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y. 
+X = data[:,0:2]
+y = data[:,2]
+
+# Create model
+model = DecisionTreeClassifier()
+
+# Fit model
+model.fit(X, y)
+
+y_pred = model.predict(X)
+
+acc = accuracy_score(y, y_pred)
