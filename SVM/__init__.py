68ea3439e3dbcb58d21ecab7aab99ee12dfff496
Farid Saud <faridsaud93@gmail.com>
2019-05-15 16:09:51 -0500

    [Feat]: Add SVM example

---
+++ SVM/__init__.py
@@ -0,0 +1,33 @@
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import pandas as pd
+import numpy as np
+
+# Read the data.
+data = np.asarray(pd.read_csv('data.csv', header=None))
+# Assign the features to the variable X, and the labels to the variable y.
+X = data[:,0:2]
+y = data[:,2]
+
+
+# split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X,
+                                                    y,
+                                                    random_state=1)
+
+# Create model
+model = SVC(kernel='rbf', gamma=27)
+
+# Fit model
+model.fit(X_train, y_train)
+
+# Predict results
+y_pred = model.predict(X_test)
+
+# See accuracy
+
+print('Accuracy score: ', format(accuracy_score(y_test, y_pred)))
+print('Precision score: ', format(precision_score(y_test, y_pred)))
+print('Recall score: ', format(recall_score(y_test, y_pred)))
+print('F1 score: ', format(f1_score(y_test, y_pred)))
