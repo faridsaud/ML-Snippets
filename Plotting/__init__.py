144abac381b38d946cf83430f7862c73cd282170
Farid Saud <faridsaud93@gmail.com>
2019-05-16 22:05:27 -0500

    [Feat]: Add Plotting example

---
+++ Plotting/__init__.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
+
+def load_pts(csv_name):
+    data = np.asarray(pd.read_csv(csv_name, header=None))
+    X = data[:, 0:2]
+    y = data[:, 2]
+
+    plt.scatter(X[np.argwhere(y == 0).flatten(), 0], X[np.argwhere(y == 0).flatten(), 1], s=50, color='blue',
+                edgecolor='k')
+    plt.scatter(X[np.argwhere(y == 1).flatten(), 0], X[np.argwhere(y == 1).flatten(), 1], s=50, color='red',
+                edgecolor='k')
+
+    plt.xlim(-2.05, 2.05)
+    plt.ylim(-2.05, 2.05)
+    plt.grid(False)
+    plt.tick_params(
+        axis='x',
+        which='both',
+        bottom='off',
+        top='off')
+
+    return X, y
+
+
+def plot_model(X, y, clf):
+    plt.scatter(X[np.argwhere(y==0).flatten(),0],X[np.argwhere(y==0).flatten(),1],s = 50, color = 'blue', edgecolor = 'k')
+    plt.scatter(X[np.argwhere(y==1).flatten(),0],X[np.argwhere(y==1).flatten(),1],s = 50, color = 'red', edgecolor = 'k')
+
+    plt.xlim(-2.05,2.05)
+    plt.ylim(-2.05,2.05)
+    plt.grid(False)
+    plt.tick_params(
+    axis='x',
+    which='both',
+    bottom='off',
+    top='off')
+
+    r = np.linspace(-2.1,2.1,300)
+    s,t = np.meshgrid(r,r)
+    s = np.reshape(s,(np.size(s),1))
+    t = np.reshape(t,(np.size(t),1))
+    h = np.concatenate((s,t),1)
+
+    z = clf.predict(h)
+
+    s.shape = (np.size(r),np.size(r))
+    t.shape = (np.size(r),np.size(r))
+    z.shape = (np.size(r),np.size(r))
+
+    plt.contourf(s,t,z,colors = ['blue','red'],alpha = 0.2,levels = range(-1,2))
+    if len(np.unique(z)) > 1:
+        plt.contour(s,t,z,colors = 'k', linewidths = 2)
+    plt.show()
+
+
+# Usage example
+X, y = load_pts('data.csv')
+plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf = DecisionTreeClassifier(random_state=42)
+
+clf.fit(X_train, y_train)
+
+plot_model(X, y, clf)
+
+
