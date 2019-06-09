2dca8f553d84bc9a8094c92fe310ba822ec2108a
Farid Saud <faridsaud93@gmail.com>
2019-06-09 17:34:28 -0500

    [Feat]: Evaluate model

--- DeepLearning/PytorchNeuralNetwork/Classifier.py
+++ DeepLearning/PytorchNeuralNetwork/Classifier.py
@@ -3,7 +3,7 @@ from torch import nn
 
 
 # Implementaion of a neural network using neural_network.png as reference
-class Network(nn.Module):
+class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
         # Inputs to hidden layer 1 linear transformation
@@ -13,18 +13,27 @@ class Network(nn.Module):
         # Output layer, 10 units - one for each digit
         self.output = nn.Linear(64, 10)
 
+        # Dropout module with 0.2 drop probability
+        self.dropout = nn.Dropout(p=0.2)
+
     def forward(self, x):
-        # Hidden layer 1 with sigmoid activation
-        x = F.relu(self.hidden(x))
-        # Hidden layer 2 with sigmoid activation
-        x = F.relu(self.hidden(x))
+        # Hidden layer 1 with dropout and sigmoid activation
+        x = self.dropout(F.relu(self.hidden_1(x)))
+        # Hidden layer 2 with dropout and sigmoid activation
+        x = self.dropout(F.relu(self.hidden_2(x)))
+
+        # To match neural_network.png
         # Output layer with softmax activation
-        x = F.softmax(self.output(x), dim=1)
+        # x = F.softmax(self.output(x), dim=1)
+
+
+        # Output layer with log_softmax activation
+        x = F.log_softmax(self.output(x), dim=1)
 
         return x
 
 
-model_1 = Network()
+model_1 = Classifier()
 
 print(model_1.hidden_1.weight)
 print(model_1.hidden_2.bias)
--- DeepLearning/PytorchNeuralNetwork/__init__.py
+++ DeepLearning/PytorchNeuralNetwork/__init__.py
@@ -3,35 +3,45 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
+from Classifier import Classifier
+import helper
 
 # Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5,), (0.5,))
                                 ])
+
+## LOAD DATA
 # Download and load the training data
-trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
+# Download and load the test data
+testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
-# define model/classifier
-model = nn.Sequential(nn.Linear(784, 128),
-                      nn.ReLU(),
-                      nn.Linear(128, 64),
-                      nn.ReLU(),
-                      nn.Linear(64, 10),
-                      nn.LogSoftmax(dim=1))
 
-# define criterion
+
+
+## DEFINE MODEL
+# instantiate model
+model = Classifier()
+
+# define criterion (if LogSoftmax => nn.NLLoss(); if softmax => nn.CrossEntropyLoss()
 criterion = nn.NLLLoss()
 
 # define optimizer
-optimizer = optim.SGD(model.parameters(), lr=0.003)
+optimizer = optim.Adam(model.parameters(), lr=0.003)
+# optimizer = optim.SGD(model.parameters(), lr=0.003)
 
+
+## TRAIN MODEL
 epochs = 5
+train_losses, test_losses = [], []
 for e in range(epochs):
     running_loss = 0
     for images, labels in trainloader:
-        # Flatten MNIST images into a 784 long vector
+        # Flatten MNIST-Fashion images into a 784 long vector
         images = images.view(images.shape[0], -1)
 
         # Clear the gradients, do this because gradients are accumulated
@@ -44,4 +54,52 @@ for e in range(epochs):
 
         running_loss += loss.item()
     else:
-        print(f"Training loss: {running_loss / len(trainloader)}")
+        test_loss = 0
+        accuracy = 0
+
+        # Turn off gradients for validation, saves memory and computations
+        with torch.no_grad():
+            model.eval()
+            for images, labels in testloader:
+                images = images.view(images.shape[0], -1)
+                log_ps = model(images)
+                test_loss += criterion(log_ps, labels)
+
+                ps = torch.exp(log_ps)
+                top_p, top_class = ps.topk(1, dim=1)
+                equals = top_class == labels.view(*top_class.shape)
+                accuracy += torch.mean(equals.type(torch.FloatTensor))
+        # Set model in training mode (dropout is enabled)
+        model.train()
+
+        train_losses.append(running_loss / len(trainloader))
+        test_losses.append(test_loss / len(testloader))
+
+        print("Epoch: {}/{}.. ".format(e + 1, epochs),
+              "Training Loss: {:.3f}.. ".format(running_loss / len(trainloader)),
+              "Test Loss: {:.3f}.. ".format(test_loss / len(testloader)),
+              "Test Accuracy: {:.3f}".format(accuracy / len(testloader)))
+
+
+
+
+## EVALUATE MODEL
+# Set model in evaluation mode (dropout is disabled)
+model.eval()
+
+images, labels = next(iter(trainloader))
+img = images[0]
+# Convert 2D image to 1D vector
+img = img.view(1, 784)
+
+# Calculate the class probabilities (softmax) for img
+with torch.no_grad():
+    output = model.forward(img)
+
+ps = torch.exp(output)
+
+helper.view_classify(img.view(1, 28, 28), ps, version='Fashion')
+
+
+
+
